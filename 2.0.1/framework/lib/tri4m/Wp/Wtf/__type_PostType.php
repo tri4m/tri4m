08651cb394d73ac95d5e4f96ed5bb749aa9982fe
icro <icro@illi.be>
2013-05-19 19:25:48 +0200

    taxonomy

---
+++ 2.0.1/framework/lib/tri4m/Wp/Wtf/__type_PostType.php
@@ -0,0 +1,30 @@
+<?PHP
+	NAMESPACE tri4m\Wp\Wtf;
+	USE tri4m\Wp\Wtf\__type_Name;
+	USE tri4m\Wp\Wtf\PostType\__type_Env;
+	USE tri4m\Wp\Wtf\PostType\__type_UpdateMsg;
+	USE ILLI\Core\Std\Def\__const_Type;
+	
+	CLASS __type_PostType EXTENDS \ILLI\Core\Std\Def\ADVTuple
+	{
+		CONST name		= 0x00;
+		CONST env		= 0x01;
+		CONST updateMsg		= 0x02;
+		
+		public function __construct(array $__setup = [])
+		{
+			parent::__construct
+			(
+				[
+					self::name		=> ['tri4m\Wp\Wtf\__type_Name'],
+					self::env		=> ['tri4m\Wp\Wtf\PostType\__type_Env'],
+					self::updateMsg		=> ['tri4m\Wp\Wtf\PostType\__type_UpdateMsg'],
+				],
+				parent::mergeOffsetValues($__setup, [
+					self::name		=> new __type_Name,
+					self::env		=> new __type_Env,
+					self::updateMsg		=> new __type_UpdateMsg
+				])
+			);
+		}
+	}
